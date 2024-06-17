6d8a362c61571b3b6d55befaade86f5880601184
matheus abrantes <abranteselcio@gmail.com>
2024-06-17 20:49:32 -0300

    refactor: using right generateId import

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { generateId } from 'src/shared/infra/utils/generate.util';
+import { generateId } from 'src/shared/utils/generate.util'
 import { Client, clientDocument } from './schema/client.entity';
 
 @Injectable()
