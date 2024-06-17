026f9a4bf24951f43ef57f56e0869f7f9047841f
Matheus Abrantes <matheus.abrantes@focustextil.com.br>
2024-06-17 08:18:38 -0300

    docs: setup swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4242);
+  await app.listen(process.env.USER_PORT);
+
+  const config = new DocumentBuilder()
+  .setTitle('FromHel Studio Backend Documentation')
+  .setDescription('Routes')
+  .setVersion('0.0.1')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  void app.listen(process.env.USER_PORT)
+
 }
 bootstrap();
