a1c32cdf3cfd6ffef5e76b983f6e5d02559688cc
mtab-dev <abranteselcio@gmail.com>
2024-06-18 17:24:50 -0300

    feat: resetDB route

---
+++ src/clients/application/controllers/resetDatabase.controller.ts
@@ -0,0 +1,21 @@
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger'
+import { Controller, Delete, Param} from '@nestjs/common';
+import { ClientService } from '../../domain/services/clients.service';
+import { CreateClientDto } from '../dto/create-client.dto';
+
+@Controller()
+export class resetDatabase {
+    constructor(private readonly clientService: ClientService) { }
+
+    @Delete('resetFromhelClientsDB')
+    @ApiTags('Delete Methods')
+    @ApiResponse({
+        status: 200,
+        description: 'All clients deleted succesfully'
+    })
+
+    resetData(){
+        return this.clientService.clientReset()
+    }
+
+}
