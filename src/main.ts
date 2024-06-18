cf108bbd4bbe69edf21c801de2124419813b6c11
matheus abrantes <abranteselcio@gmail.com>
2024-06-17 22:17:03 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  
+  app.enableCors()
+
   const config = new DocumentBuilder()
   .setTitle('FromHel Studio Backend Documentation')
   .setDescription('Routes')
