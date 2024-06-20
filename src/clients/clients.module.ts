40088a49f69ab83eef2d09d9e759a6ac1d2e3298
matheus <96802002+mtab-dev@users.noreply.github.com>
2024-06-20 13:26:54 -0300

    refactor: remove mailer service

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,4 +1,3 @@
-import { MailerController } from './application/controllers/mailer.controller';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ClientService } from './domain/services/clients.service';
@@ -8,34 +7,19 @@ import { findEmailController } from './application/controllers/findEmail.control
 import { registerClientController } from './application/controllers/registerClient.controller';
 import { IdFindController } from './application/controllers/clientsById.controller';
 import { removeClientController } from './application/controllers/removeClient.controller';
-import { MailerModule } from '@nestjs-modules/mailer'
 
 @Module({
   imports: [  
     MongooseModule.forFeature([
       { name: Client.name, schema: ClientSchema },
-    ]),
-    MailerModule.forRoot({
-      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
-      defaults: {
-        from: '"nest-modules" <modules@nestjs.com>',
-      },
-      template: {
-        dir: __dirname + '/templates',
-        adapter: new PugAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
+    ])
   ],
   controllers: [
     findClientController,
     findEmailController,
     registerClientController,
     IdFindController,
-    removeClientController,
-    MailerController
+    removeClientController
   ],
   providers: [ClientService],
 })
