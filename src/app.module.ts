cf03a5a9cee721186b933e6d25fb13866102b0dd
matheus <96802002+mtab-dev@users.noreply.github.com>
2024-06-21 17:30:53 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ClientsModule } from './clients/clients.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CorsModule } from '@nestjs/common';
 import { ENVIROMENT } from './config/env';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    CorsModule.forRoot({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: '*',
-    }),
     MongooseModule.forRoot(ENVIROMENT.MONGO_URL_PRD,
       {dbName: ENVIROMENT.MONGO_CONNECTION_DATABASE}
     ),
