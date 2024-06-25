f950525e6a660fa84e6ca0aff2887d04d9c25f63
matheus <96802002+mtab-dev@users.noreply.github.com>
2024-06-25 10:43:43 -0300

    ci: cors configuration

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,9 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: '*',
-  });
   
   const config = new DocumentBuilder()
   .setTitle('FromHel Studio Backend Documentation')
